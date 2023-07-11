a14d0726784ee1f1ff08fb2996d60f7a4aad5c4d
xxxrePAPRIKA <xxxre.paprika@gmail.com>
2023-07-11 21:47:03 +0700

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,21 @@ window.addEventListener('DOMContentLoaded', function () {
     burger('.header__menu_burger', '.burger')
     
 });
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+
+    let formSliderState = {};
+    formSlider('.form-slider__navigation_prev .button_black', '.form-slider__navigation_next .button_black', '.form-slider__item', '.form-slider__wrapper', '.form-slider__inner');
+    changeFormSliderSate(formSliderState);
+    examplesSlider('.examples__navigation .navigation_prev .button_black', '.examples__navigation .navigation_next .button_black', '.examples__slide', '.examples__wrapper', '.examples__inner' );
+    reviewsSlider('.reviews__navigation .navigation_prev .button_black', '.reviews__navigation .navigation_next .button_black', '.reviews__slide', '.reviews__wrapper', '.reviews__inner'); // init slider for reviews
+    accordion('.questions__accordion__item', '.questions__accordion__descr');
+    forms(formSliderState);
+    modals();
+    mask('[name="phone"]');
+    burger('.header__menu_burger', '.burger')
+    
+});
